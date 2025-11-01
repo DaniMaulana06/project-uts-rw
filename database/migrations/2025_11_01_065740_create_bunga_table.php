f51c93ce540187ebd37cd3a34de6651b1798cd19
DaniMaulana06 <dani24maulana@gmail.com>
2025-11-01 15:35:48 +0700

    rename migration bunga

--- database/migrations/2025_11_01_065740_create_bunga_table.php
+++ database/migrations/2025_11_01_065740_create_bunga_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('bungas', function (Blueprint $table) {
+        Schema::create('bunga', function (Blueprint $table) {
             $table->id();
             $table->string('gambar');
             $table->string('nama');
